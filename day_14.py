6e3049649e89ccd57f1dd403b3497e7899b2cf2d
Christopher Dembski <christopher_dembski@outlook.com>
2022-12-14 17:54:54 -0500

    Day 14. Part 2.

--- day_14.py
+++ day_14.py
@@ -1,11 +1,16 @@
 import re
 from itertools import pairwise, chain
 
+PART = 2
+
 
 class SandCave:
 
     def __init__(self, file_path):
         self.grid, left_border = self.parse_input(file_path)
+        if PART == 2:
+            self.grid.append(['.' for _ in range(len(self.grid[0]))])
+            self.grid.append(['#' for _ in range(len(self.grid[0]))])
         self.sand_start_r = 0
         self.sand_start_c = 500 - left_border
 
@@ -13,12 +18,17 @@ class SandCave:
         count = 0
         while self.drop_sand():
             count += 1
-        return count
+        return count if PART == 1 else count + 1
 
     def drop_sand(self):
         sand_r = self.sand_start_r
         sand_c = self.sand_start_c
         while True:
+            if PART == 2:
+                if sand_c == len(self.grid[0]) - 1:
+                    self.add_colum_right()
+                elif sand_c == 0:
+                    self.add_column_left()
             if self.within_grid(sand_r + 1, sand_c) and self.grid[sand_r + 1][sand_c] == '.':
                 sand_r += 1
             elif self.within_grid(sand_r + 1, sand_c - 1) and self.grid[sand_r + 1][sand_c - 1] == '.':
@@ -28,11 +38,29 @@ class SandCave:
                 sand_r += 1
                 sand_c += 1
             else:
-                if sand_c == 0 or sand_c == len(self.grid[0]) - 1 or sand_r == len(self.grid) - 1:
-                    return False
-                else:
-                    self.grid[sand_r][sand_c] = 'o'
-                    return True
+                if PART == 1:
+                    if sand_c == 0 or sand_c == len(self.grid[0]) - 1 or sand_r == len(self.grid) - 1:
+                        return False
+                    else:
+                        self.grid[sand_r][sand_c] = 'o'
+                        return True
+                else:  # PART == 2
+                    if sand_r == 0:
+                        return False
+                    else:
+                        self.grid[sand_r][sand_c] = 'o'
+                        return True
+
+    def add_colum_right(self):
+        for row in self.grid:
+            row.append('.')
+        self.grid[-1][-1] = '#'
+
+    def add_column_left(self):
+        for r, row in enumerate(self.grid):
+            self.grid[r] = ['.'] + row
+        self.grid[-1][0] = '#'
+        self.sand_start_c += 1
 
     def within_grid(self, r, c):
         return 0 <= r < len(self.grid) and 0 <= c < len(self.grid[0])
@@ -52,16 +80,9 @@ class SandCave:
             right_border = max(max(c for c, r in chain(*all_paths)), 500)
             top_border = 0  # sand start at 0
             bottom_border = max(r for c, r in chain(*all_paths))
-            # print('all paths', all_paths)
-            # print('left', left_border)
-            # print('right', right_border)
-            # print('top', top_border)
-            # print('bottom', bottom_border)
             grid = [['.' for c in range(left_border, right_border + 1)]
                     for r in range(top_border, bottom_border + 1)]
-            # print('\n'.join(''.join(ch for ch in line) for line in grid))
             SandCave.populate_grid(all_paths, grid, left_border)
-            # print('\n'.join(''.join(ch for ch in line) for line in grid))
             return grid, left_border
 
     @staticmethod
@@ -83,3 +104,4 @@ class SandCave:
 # note: working for example input but not real data
 sand_cave = SandCave('inputs/day_14/data.txt')
 print(sand_cave.solve())
+sand_cave.display()
