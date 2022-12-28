710d6c94054632e4af30ec290b6ad3b42025a079
Christopher Dembski <christopher_dembski@outlook.com>
2022-12-27 23:35:25 -0500

    Day 20. Part 1.

--- day_20.py
+++ day_20.py
@@ -14,54 +14,62 @@ class Node:
         print(' -> '.join(str(n) for n in numbers))
 
     def __repr__(self):
-        return f'Node({self.value}, prev={self.previous_node.value}, next={self.next_node.value})'
+        return f'Node(value={self.value}, prev={self.previous_node.value}, next={self.next_node.value})'
 
 
 def parse_input(file_path):
-    node_dict = {}
+    nodes = []
     with open(file_path) as file:
         numbers = iter(int(n) for n in file.read().splitlines())
         head = Node(next(numbers), None, None)
-        node_dict[head.value] = head
+        nodes.append(head)
         previous_node = head
         for n in numbers:
             current_node = Node(n, previous_node, None)
-            node_dict[n] = current_node
+            nodes.append(current_node)
             previous_node.next_node = current_node
             previous_node = current_node
         head.previous_node = previous_node
         previous_node.next_node = head
-        return node_dict, head
+        return nodes
 
 
-def part_1(file_path):
-    node_dict, linked_list = parse_input(file_path)
-    for number, to_move in node_dict.items():
-        if number == 0:
+def mix(nodes):  # MUTATES: linked_list
+    for to_move in nodes:
+        if to_move.value == 0:
             continue
         to_move.previous_node.next_node = to_move.next_node
         to_move.next_node.previous_node = to_move.previous_node
         current_node = to_move
-        if number > 0:
-            for step in range(number):
+        if to_move.value > 0:
+            for step in range(to_move.value):
                 current_node = current_node.next_node
-        else:  # number < 0
-            for step in range(abs(number) + 1):
+        else:  # to_move.value < 0
+            for step in range(abs(to_move.value) + 1):
                 current_node = current_node.previous_node
         next_node = current_node.next_node
         current_node.next_node = to_move
         to_move.previous_node = current_node
         to_move.next_node = next_node
         next_node.previous_node = to_move
+
+
+def sum_grove_coordinates(nodes):
     result = 0
-    current_node = linked_list
+    current_node = nodes[0]
     while current_node.value != 0:
         current_node = current_node.next_node
     for i in range(1, 3001):
         current_node = current_node.next_node
         if i in (1000, 2000, 3000):
             result += current_node.value
-    print(result)
+    return result
 
 
-part_1('inputs/day_20/example_data.txt')
+def part_1(file_path):
+    nodes = parse_input(file_path)
+    mix(nodes)  # MUTATION: linked_list
+    return sum_grove_coordinates(nodes)
+
+
+print(part_1('inputs/day_20/data.txt'))
